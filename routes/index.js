979e3aecd50b774e136246a76411b6e9dc950e6a
jiajiaanan <jiajiaanan@gmail.com>
2022-11-23 20:47:06 +0800

    feat:signup page & usercontroller

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
