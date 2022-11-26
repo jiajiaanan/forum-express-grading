bdada71423366f23c1b1aaeeeef65367eac1cf30
jiajiaanan <jiajiaanan@gmail.com>
2022-11-26 09:17:51 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const passport = require('../config/passport')
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
