a10ebdf2d8e72d730d98a27e774bcd7b1f22bec8
jiajiaanan <jiajiaanan@gmail.com>
2022-11-30 20:22:48 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,17 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50) // 將description擷取前五十字元覆蓋原始的description
+      }))
+      return res.render('restaurants', { restaurants: data })
+    })
   }
 }
 module.exports = restaurantController
