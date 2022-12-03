79691e4cecdade0b2e31b88ae78c3aea0f80889a
jiajiaanan <jiajiaanan@gmail.com>
2022-12-03 10:09:33 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,10 +1,13 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 const hbshelpers = require('handlebars-helpers')
+const relativeTime = require('dayjs/plugin/relativeTime') // 增加這裡
+dayjs.extend(relativeTime)
 const multihelpers = hbshelpers()
 
 module.exports = {
   multihelpers,
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
