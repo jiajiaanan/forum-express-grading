31c83a92760b049c97513b45878343d7839970cd
jiajiaanan <jiajiaanan@gmail.com>
2022-12-06 20:48:10 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 router.post('/signup', userController.signUp)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.put('/users/:id', upload.single('image'), userController.putUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
