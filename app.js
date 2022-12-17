797de1185e24318f4bfc900ee36a175aebe977e4
jiajiaanan <jiajiaanan@gmail.com>
2022-12-17 10:22:19 +0800

    feat: add api-auth middleware

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
@@ -9,10 +12,6 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const app = express()
 const port = process.env.PORT || 3000
